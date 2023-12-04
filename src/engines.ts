53c4fc4242af02ea515735a3db206a7dbd8a33d4
AlfieRan <alfie.ranstead@outlook.com>
2023-12-04 10:21:04 +0000

    feat: gmail and gcal support

--- src/engines.ts
+++ src/engines.ts
@@ -3,17 +3,17 @@ import { z } from 'zod';
 type EngineData = {
 	baseUrl: string;
 	searchUrl: string;
-}
+};
 
 // To add a new engine, add the engine name to the array below, then add the engine to the engines object.
 
 const availableEngines = ['google', 'duckduckgo', 'bing'] as const;
-type AvailableEngines = typeof availableEngines[number];
+type AvailableEngines = (typeof availableEngines)[number];
 
-export const engines: { [engine: AvailableEngines]: EngineData } = {
-	'bing': { searchUrl: 'https://www.bing.com/search?q=%s', baseUrl: 'https://bing.com' },
-	'duckduckgo': { searchUrl: 'https://duckduckgo.com/?q=%s', baseUrl: 'https://duckduckgo.com' },
-	'google': { searchUrl: 'https://www.google.com/search?q=%s', baseUrl: 'https://google.com' },
+export const engines: { [engine in AvailableEngines]: EngineData } = {
+	bing: { searchUrl: 'https://www.bing.com/search?q=%s', baseUrl: 'https://bing.com' },
+	duckduckgo: { searchUrl: 'https://duckduckgo.com/?q=%s', baseUrl: 'https://duckduckgo.com' },
+	google: { searchUrl: 'https://www.google.com/search?q=%s', baseUrl: 'https://google.com' },
 };
 
 // Please keep any new engines in alphabetical order and in between this comment and the one above.
--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -4,62 +4,87 @@ type ShortcutData = {
 	baseUrl: string;
 	searchUrl: string;
 	accountUrl?: string;
-}
+};
 
 // SHORTCUTS ==========================================================================================================
 
 // Start by adding the aliases for your shortcut here, they're split into multiple arrays for organization so please
 // keep them that way :)
-const socialShortcuts = ['insta', 'instagram', 'twt', 'twitter', 'yt', 'youtube', 'twitch', 'linkedin']  as const;
+const socialShortcuts = ['insta', 'instagram', 'twt', 'twitter', 'yt', 'youtube', 'twitch', 'linkedin'] as const;
 const devShortcuts = ['github', 'gh', 'lin', 'linear', 'vercel', 'fly'] as const;
 const streamingShortcuts = ['netflix', 'prime'] as const;
 const shoppingShortcuts = ['amazon', 'ebay'] as const;
-const miscShortcuts = ['gdrive'] as const;
-
+const miscShortcuts = ['gdrive', 'gmail', 'gcal'] as const;
 
 // If you create a new organization array, add it to this array
-const availableShortcuts = [...socialShortcuts, ...devShortcuts, ...streamingShortcuts, ...shoppingShortcuts, ...miscShortcuts]  as const;
-type AvailableShortcuts = typeof availableShortcuts[number];
+const availableShortcuts = [...socialShortcuts, ...devShortcuts, ...streamingShortcuts, ...shoppingShortcuts, ...miscShortcuts] as const;
+type AvailableShortcuts = (typeof availableShortcuts)[number];
 
 // If you want to add multiple aliases for a shortcut, define the shortcut data here
-const GithubData = { baseUrl: 'https://www.github.com/', searchUrl: 'https://github.com/search?q=%s', accountUrl: 'https://github.com/%s' }
-const InstagramData = { baseUrl: 'https://instagram.com', searchUrl: 'https://www.instagram.com/explore/tags/%s', accountUrl: 'https://www.instagram.com/%s/' };
+const GithubData = { baseUrl: 'https://www.github.com/', searchUrl: 'https://github.com/search?q=%s', accountUrl: 'https://github.com/%s' };
+const InstagramData = {
+	baseUrl: 'https://instagram.com',
+	searchUrl: 'https://www.instagram.com/explore/tags/%s',
+	accountUrl: 'https://www.instagram.com/%s/',
+};
 const TwitterData = { baseUrl: 'https://twitter.com', searchUrl: 'https://twitter.com/search?q=%s', accountUrl: 'https://twitter.com/%s' };
-const YoutubeData = { baseUrl: 'https://youtube.com', searchUrl: 'https://www.youtube.com/results?search_query=%s',  accountUrl: "https://youtube.com/@%s" };
+const YoutubeData = {
+	baseUrl: 'https://youtube.com',
+	searchUrl: 'https://www.youtube.com/results?search_query=%s',
+	accountUrl: 'https://youtube.com/@%s',
+};
 
 const LinearData = { baseUrl: 'https://linear.app', searchUrl: 'https://linear.app/%s', accountUrl: 'https://linear.app/%s' };
+
+const GmailData = {
+	baseUrl: 'https://mail.google.com/mail/u/0/#inbox',
+	searchUrl: 'https://mail.google.com/mail/u/0/#search/%s',
+	accountUrl: 'https://mail.google.com/mail/u/%s/#inbox',
+};
+
+const GcalendarData = {
+	baseUrl: 'https://calendar.google.com/calendar/u/0/r',
+	searchUrl: 'https://calendar.google.com/calendar/u/0/r',
+	accountUrl: 'https://calendar.google.com/calendar/u/%s/r',
+};
 
 // Then map your aliases to the shortcut data in this object
 export const shortcuts: Record<AvailableShortcuts, ShortcutData> = {
 	// Social
-	'insta': InstagramData,
-	'instagram': InstagramData,
-	'linkedin': { baseUrl: 'https://www.linkedin.com', searchUrl: 'https://www.linkedin.com/search/results/all/?keywords=%s', accountUrl: "https://www.linkedin.com/search/results/people/?keywords=%s" },
-	'twt': TwitterData,
-	'twitter': TwitterData,
-	'twitch': { baseUrl: 'https://www.twitch.tv', searchUrl: 'https://www.twitch.tv/search?term=%s', accountUrl: "https://www.twitch.tv/%s" },
-	'yt': YoutubeData,
-	'youtube': YoutubeData,
+	insta: InstagramData,
+	instagram: InstagramData,
+	linkedin: {
+		baseUrl: 'https://www.linkedin.com',
+		searchUrl: 'https://www.linkedin.com/search/results/all/?keywords=%s',
+		accountUrl: 'https://www.linkedin.com/search/results/people/?keywords=%s',
+	},
+	twt: TwitterData,
+	twitter: TwitterData,
+	twitch: { baseUrl: 'https://www.twitch.tv', searchUrl: 'https://www.twitch.tv/search?term=%s', accountUrl: 'https://www.twitch.tv/%s' },
+	yt: YoutubeData,
+	youtube: YoutubeData,
 
 	// Dev
-	'github': GithubData,
-	'gh': GithubData,
-	'linear': LinearData,
-	'lin': LinearData,
-	'vercel': { baseUrl: 'https://vercel.com/', searchUrl: 'https://vercel.com/', accountUrl: 'https://vercel.com/%s' },
-	'fly': { baseUrl: 'https://fly.io/dashboard', searchUrl: 'https://fly.io/dashboard', accountUrl: 'https://fly.io/dashboard/%s' },
+	github: GithubData,
+	gh: GithubData,
+	linear: LinearData,
+	lin: LinearData,
+	vercel: { baseUrl: 'https://vercel.com/', searchUrl: 'https://vercel.com/', accountUrl: 'https://vercel.com/%s' },
+	fly: { baseUrl: 'https://fly.io/dashboard', searchUrl: 'https://fly.io/dashboard', accountUrl: 'https://fly.io/dashboard/%s' },
 
 	// Streaming
-	'netflix': { baseUrl: 'https://www.netflix.com', searchUrl: 'https://www.netflix.com/search?q=%s' },
-	'prime': { baseUrl: 'https://www.primevideo.com', searchUrl: 'https://www.primevideo.com/search/phrase=%s' },
+	netflix: { baseUrl: 'https://www.netflix.com', searchUrl: 'https://www.netflix.com/search?q=%s' },
+	prime: { baseUrl: 'https://www.primevideo.com', searchUrl: 'https://www.primevideo.com/search/phrase=%s' },
 
 	// Shopping
-	'amazon': { baseUrl: 'https://www.amazon.co.uk', searchUrl: 'https://www.amazon.co.uk/s?k=%s' },
-	'ebay': { baseUrl: 'https://www.ebay.co.uk', searchUrl: 'https://www.ebay.co.uk/sch/i.html?_nkw=%s' },
+	amazon: { baseUrl: 'https://www.amazon.co.uk', searchUrl: 'https://www.amazon.co.uk/s?k=%s' },
+	ebay: { baseUrl: 'https://www.ebay.co.uk', searchUrl: 'https://www.ebay.co.uk/sch/i.html?_nkw=%s' },
 
 	// Misc
-	'gdrive': { baseUrl: 'https://drive.google.com', searchUrl: 'https://drive.google.com/drive/search?q=%s' },
-}
+	gdrive: { baseUrl: 'https://drive.google.com', searchUrl: 'https://drive.google.com/drive/search?q=%s' },
+	gmail: GmailData,
+	gcal: GcalendarData,
+};
 
 // SHORTCUT PARSING ===================================================================================================
 
